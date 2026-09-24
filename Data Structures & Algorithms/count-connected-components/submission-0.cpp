724b989a6795c5a8163db4d595206ee9b04125a2
ShaikKoduruAsif114 <ce23b114@smail.iitm.ac.in>
2026-09-24 21:53:47 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    void dfs(int node,vector<vector<int>>&adj,vector<int>&vis){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(vis[it]==0) dfs(it,adj,vis);
+        }
+        return;
+    }
+
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<int>vis(n,0);
+        vector<vector<int>>adj(n);
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        int cnt = 0;
+        for(int i = 0;i<n;i++){
+            if(vis[i]==0){
+                dfs(i,adj,vis);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
